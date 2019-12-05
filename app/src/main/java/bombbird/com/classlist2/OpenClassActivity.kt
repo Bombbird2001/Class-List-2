7ddaea11d300a44e726bf3cd9ab1f013fc30f61d
Shang Yi Ku <kushangyi2001@gmail.com>
2019-12-05 21:52:52 +0800

    Initial commit

---
+++ app/src/main/java/bombbird/com/classlist2/OpenClassActivity.kt
@@ -0,0 +1,12 @@
+package bombbird.com.classlist2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OpenClassActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_open_class)
+    }
+}
